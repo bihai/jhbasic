f6517fb98cb97523525fba941f9ab2b189428ea4
David Betz <dbetz@xlisper.com>
2014-06-06 22:47:41 -0400

    Remove the Visual C++ project because it didn't work anyway.

--- hdr/db_types.h
+++ hdr/db_types.h
@@ -18,12 +18,12 @@ extern "C"
 
 #ifdef WIN32
 // Ugh! Visual Studio C++ doesn't supply stdint.h
-typedef char int8_t;
-typedef unsigned char uint8_t;
-typedef short int16_t;
-typedef unsigned short uint16_t;
-typedef int int32_t;
-typedef unsigned int uint32_t;
+typedef __int8 int8_t;
+typedef unsigned __int8 uint8_t;
+typedef __int16 int16_t;
+typedef unsigned __int16 uint16_t;
+typedef __int32 int32_t;
+typedef unsigned __int32 uint32_t;
 #else
 #include <stdint.h>
 #endif
